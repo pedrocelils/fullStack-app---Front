ee84f5de66ca539f7d1a195969e4b8b3b7fbde23
pedrocelils <pedroceliob3@gmail.com>
2023-02-22 20:03:38 -0300

    Commit front-end

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddUser from './users/AddUser';
+import EditUser from './users/EditUser';
+import ViewUser from './users/ViewUser';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Router>
+      <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />}></Route>
+            <Route exact path="/addUser" element={<AddUser />}></Route>
+            <Route  exact path="/edituser/:id" element={<EditUser/>} />
+            <Route exact path="/viewuser/:id" element={<ViewUser/>} />
+          </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
